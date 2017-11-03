157beb5c8dd08e9527da143cc3f2c3d409549fbe
Charles Kaminer <ckamine@g.clemson.edu>
2017-11-02 22:13:19 -0400

    split out methods and demonstrate different assignments and types

--- main.go
+++ main.go
@@ -16,36 +16,45 @@ type fooHandler struct{}
 func (f fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		cash := struct {
-			Answer string
-		} {
-			"This was a GET, cuz",
-		}
-		RespondWithJSON(w, http.StatusOK, cash)
+		GetHandler(w, r)
 	case "POST":
-		type Request struct {
-			Guess string `json:"guess"`
-		}
-		var req Request
-		err := json.NewDecoder(r.Body).Decode(&req)
-		if err != nil {
-			log.Println("Error decoding request: ", err.Error())
-			RespondWithError(w, http.StatusInternalServerError, "This request was not that tight.  I think it was your fault, not mine. Don't @ me")
-			return
-		}
-		defer r.Body.Close()
-
-		cash := struct {
-			Answer string
-		} {
-			"UHHH...YUP!!!!",
-		}
-		if req.Guess == "Tom Brady" {
-			RespondWithJSON(w, 200, cash)
-		} else {
-			RespondWithJSON(w, 200, map[string]string{"Answer": "LOL NAH"})
-		}
+		PostHandler(w, r)
 	}
+}
+
+type Response struct {
+	Message string
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	res := Response{"This was a GET, ya dig?"}
+	RespondWithJSON(w, http.StatusOK, res.Message)
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Guess string `json:"guess"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		log.Println("Error decoding request: ", err.Error())
+		RespondWithError(w, http.StatusInternalServerError, "This request was not that tight.  I think it was your fault, not mine. Don't @ me")
+		return
+	}
+	defer r.Body.Close()
+	
+	res := CheckGuess(req.Guess)
+	RespondWithJSON(w, http.StatusOK, Response{res})	
+}
+
+func CheckGuess(guess string) (answer string) {
+	if guess == "Tom Brady" {
+		answer = "UHHH...YUP!!!!"
+	} else {
+		answer = "LOL NAH"
+	}
+	return
 }
 
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
@@ -60,24 +69,3 @@ func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 func RespondWithError(w http.ResponseWriter, code int, message string) {
 	RespondWithJSON(w, code, map[string]string{"error": message})
 }
-
-
-// // BASIC
-// func (f fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	cash := struct {
-// 		Animal string
-// 		Genus string
-// 		Species string
-// 		Type string
-// 	} {
-// 		"Goat", 
-// 		"Thomas", 
-// 		"Brady", 
-// 		"Rare AF",
-// 	}
-// 	response, _ := json.Marshal(cash)
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.Header().Set("Access-Control-Allow-Origin", "*")
-// 	w.WriteHeader(200)
-// 	w.Write(response)
-// }
